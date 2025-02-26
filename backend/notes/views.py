6d12b34a39c5da483491a9ebb4059981d3532b71
Jonah Cragun <jonahcragun@gmail.com>
2025-02-25 18:02:16 -0800

    add rest api in views and note table in models, created new react template

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework.decorator import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializers import *
 
-# Create your views here.
+# get all notes 
+@api_view(['GET'])
+def create_note(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response({'status': 200, 'payload': serializer.data})
+
+# add new note
+@api_view(['POST'])
+def create_note(request):
+    serializer = NoteSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# get note content for particular pk
+@api_view(['GET'])
+def get_note(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = NoteSerializer(note)
+    return Response({'status': 200, 'payload': serializer.data})
+
+# modify note content for particular pk
+@api_view(['POST'])
+def update_contents(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    note.content = request.data
+    note.save()
+    serializer = NoteSerializer(note)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+# delete note
+@api_view(['DELETE'])
+def delete_note(request, pk):
+    try: 
+        note = Note.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    note.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
